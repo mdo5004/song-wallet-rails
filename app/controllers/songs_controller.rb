40d1dff3e536f138287c0638f338b7e5613f1a9e
Michael OConnell <michaeldavidoconnell@gmail.com>
2017-07-20 12:35:02 -0400

    basic routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,20 @@
 class SongsController < ApplicationController
+    def index
+        @songs = Song.all
+    end
+    def create
+    end
+    def new
+    end
+    def edit
+    end
+    def show
+        @song = Song.find(params[:id])
+        @song_html = Chordpro.html(@song.content)
+    end
+    def update
+    end
+    def destroy
+    end
+    
 end
