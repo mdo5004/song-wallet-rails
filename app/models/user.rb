2a437d05322d1a17e70468bd940d69a2e4415556
Michael OConnell <michaeldavidoconnell@gmail.com>
2017-08-08 13:55:29 -0400

    added group and setlist relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
     has_many :songs, through: :user_songs
     has_many :user_friends
     has_many :friends, through: :user_friends, :class_name => "User", :foreign_key => "friend_id"
+    has_many :user_groups
+    has_many :groups, through: :user_groups
+    has_many :user_setlists
+    has_many :setlists, through: :user_setlists
+    
 end
